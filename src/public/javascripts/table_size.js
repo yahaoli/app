7ac7e482fc94de17343a24e1853f6bf0afcf3ba1
李亚豪 <1151050603@qq.com>
2018-01-11 12:57:49 +0800

    app代码更新

--- src/public/javascripts/table_size.js
+++
@@ -1,7 +0,0 @@
-layui.define(['jquery'], function () {
-    var $ = layui.$;
-    $('.table-size').height(parseInt($(window).height())-190+'px');
-    $(window).on('resize',function () {
-        $('.table-size').height(parseInt($(window).height())-190+'px')
-    })
-})
